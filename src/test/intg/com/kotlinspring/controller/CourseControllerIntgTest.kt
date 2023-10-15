404199d5bbfe818be53e072fcd63895c1ac76e45
Adrian Nachev <ain00006@gmail.com>
2023-10-15 14:51:03 +0200

    - add integration test for update course endpoint

--- src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.entity.Course
 import com.kotlinspring.repository.CourseRepository
 import com.kotlinspring.util.courseEntityList
 import org.junit.jupiter.api.Assertions
@@ -66,5 +67,25 @@ class CourseControllerIntgTest {
 
         println("courseDTOs : $courseDTOs")
         assertEquals(3, courseDTOs!!.size)
+    }
+
+    @Test
+    fun updateCourse() {
+
+        val course = Course(null, "Build RestFul APis using SpringBoot and Kotlin", "Development")
+        courseRepository.save(course)
+
+        val updatedCourseDTO = CourseDTO(null, "Build RestFul APis using SpringBoot and Kotlin1", "Development")
+
+        val updatedCourse = webTestClient
+            .put()
+            .uri("/v1/courses/{courseId}", course.id)
+            .bodyValue(updatedCourseDTO)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CourseDTO::class.java)
+            .returnResult().responseBody
+
+        assertEquals("Build RestFul APis using SpringBoot and Kotlin1", updatedCourse!!.name)
     }
 }
