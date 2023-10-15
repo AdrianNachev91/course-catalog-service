b4ee819d44294582a2ea734a67047497e0a4b248
Adrian Nachev <ain00006@gmail.com>
2023-10-15 15:30:02 +0200

    - add unit tests for Course controller

--- src/test/unit/com.kotlinspring.controller/GreetingControllerUnitTest.kt
+++ src/test/unit/com.kotlinspring.controller/GreetingControllerUnitTest.kt
@@ -7,12 +7,10 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@WebMvcTest(controllers = [GreetingController::class])
 @AutoConfigureWebTestClient
 class GreetingControllerUnitTest {
 
